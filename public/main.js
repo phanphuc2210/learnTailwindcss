3efdc6e592c5811a62b713b057ac89e5c3def5a3
phanphuc2210 <phucpth2001@gmail.com>
2022-09-30 18:55:47 +0700

    Header: styling and handling the menu bar

---
+++ public/main.js
@@ -0,0 +1,7 @@
+// Menu
+const menu = document.querySelector("#menu")
+const menuBtn = document.querySelector("#menu-btn")
+
+menuBtn.addEventListener("click", ()=> {
+    menu.classList.toggle("invisible")
+})
